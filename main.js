0fa3fb8f68bfcf1f6610ed3749c8559ab1ac3e11
ayyPls <saispaev55@mail.ru>
2023-09-07 13:34:45 +0700

    attach annotations for 3d model

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
-import { Scene, PerspectiveCamera, WebGLRenderer, AmbientLight, AnimationMixer, Clock, CameraHelper, Color } from 'three';
+import { Scene, PerspectiveCamera, AxesHelper, WebGLRenderer, AmbientLight, AnimationMixer, Clock, Color, SpriteMaterial, Sprite, CanvasTexture, Group } from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
-import { CSS2DObject, CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer';
 
 // TODO: наезд камеры при первом рендере 
-// TODO: visible (and hideable on collision?) and clickable dots on model
 // TODO: background of scene (skybox?) https://threejs.org/manual/#en/backgrounds
 
-let mixer, floatingAnimation, shootingAnimation, annotations;
+let mixer, floatingAnimation, shootingAnimation;
 
-const loader = new GLTFLoader()
 const clock = new Clock()
 
 const scene = new Scene();
 scene.background = new Color(0xc5c5c5) // set scene background color
 
 const camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 3 // set camera position
+// set camera position
+camera.position.x = -3
+camera.position.y = 1
+camera.position.z = -3
+
+const axesHelper = new AxesHelper(5);
+scene.add(axesHelper);
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
@@ -25,32 +28,42 @@ document.body.appendChild(stats.dom)
 
 const renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.domElement.className = 'renderer'
 document.body.appendChild(renderer.domElement);
-
-const labelRenderer = new CSS2DRenderer()
-labelRenderer.domElement.style.position = 'absolute';
-labelRenderer.domElement.style.top = '0px';
-labelRenderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(labelRenderer.domElement)
-
 
 const light = new AmbientLight(0x404040, 100); // soft white light
 scene.add(light);
 
-
-const cameraHelper = new CameraHelper(camera)
-scene.add(cameraHelper)
-
-
-const controls = new OrbitControls(camera, labelRenderer.domElement) // bind controls to 2d renderer to work if you have some 2d to render
+const controls = new OrbitControls(camera, renderer.domElement) // bind controls to 2d renderer to work if you have some 2d to render
 controls.enablePan = false; // disable moving model with ctrl+mouse
 controls.enableDamping = true; // smooth camera rotation
 controls.dampingFactor = 0.05;
 controls.maxDistance = 5; // min and max distance for camera zoom
 controls.minDistance = 1.5;
 
+let canvas = document.createElement('canvas');
+canvas.width = 64;
+canvas.height = 64; let context = canvas.getContext('2d');
+context.lineStyle = 'black';
+context.lineWidth = 6;
+context.fillStyle = 'white';
+context.beginPath();
+context.arc(32, 32, 24, 0, 2 * Math.PI);
+context.fill();
+context.stroke();
+const circleTexture = new CanvasTexture(canvas)
+const annotationSpriteMaterial = new SpriteMaterial({
+    transparent: true,
+    opacity: 1,
+    depthTest: true,
+    map: circleTexture,
+})
+
+let sprites = new Group();
 
 const modelUrl = '/models/wither_boss/source/witherBoss.gltf';
+
+const loader = new GLTFLoader()
 loader.load(
     modelUrl,
     model => {
@@ -58,64 +71,45 @@ loader.load(
         // bind animations
         floatingAnimation = model.animations[0]
         shootingAnimation = model.animations[1]
-        mixer.clipAction(shootingAnimation).play()
-        // rotate model to another angle
+        mixer.clipAction(floatingAnimation).play().timeScale = 0.5
         model.scene.position.y = -1.3
-        model.scene.rotateY(2.5)
-        annotations = model.scene.children.flatMap(obj => obj.children.filter(part => part.isMesh == true))
-        // console.log(annotations);
-        // renderAnnotations(annotations)
-
+        model.scene.children.map(obj => obj.children.filter(part => !part.isMesh).map((part, index) => {
+            const annotationSprite = new Sprite(annotationSpriteMaterial)
+            annotationSprite.scale.set(0.05, 0.05, 0.05)
+            annotationSprite.position.copy({ ...part.position, y: part.position.y - 1, z: part.position.z - 0.3 })
+            // add sprite into group
+            sprites.add(annotationSprite)
+        }))
+        // attach group of sprites to model 
+        model.scene.children[1].attach(sprites)
         scene.add(model.scene)
-        const labelElement = document.createElement("div")
-        labelElement.className= 'label'
-        labelElement.textContent = 'label'
-        labelElement.style.marginTop = '-1em'
-        labelElement.style.background = 'none'
-
-        const label = new CSS2DObject(labelElement)
-        annotations[0].add(label)
-        // const label = annotations[0]
     },
-    undefined,
+    () => {
+        // on progress
+    },
     error => console.error(error)
 )
-
-
-// const renderAnnotations = (annotations) => {
-//     annotations.map(
-//         annotation => {
-//             const hotspot = document.createElement("div")
-//             hotspot.className = "hotspot"
-//             hotspot.setAttribute("name", annotation.name)
-//             // add tooltip
-//             const tooltip = document.createElement("div")
-//             tooltip.className = "tooltip"
-//             // set text
-//             tooltip.innerHTML = annotation.userData.name
-//             hotspot.appendChild(tooltip)
-
-//             const hotspotLabel = new CSS2DObject(hotspot)
-//             hotspotLabel.position.set(0, 0, 0)
-//             annotation.add(hotspotLabel)
-//             hotspotLabel.layers.set(0)
-//             scene.add(hotspotLabel)
-//             console.log(hotspotLabel);
-//         })
-// }
-
 
 function animate() {
     requestAnimationFrame(animate);
     const delta = clock.getDelta()
     controls.update()
-    stats.update()
-    const elapsed = clock.getElapsedTime()
+    stats.update();
 
     // run animation
-    if (mixer)
-        mixer.update(delta)
+    if (mixer) mixer.update(delta)
+
+    // render scene + all sprites as transparent
+    renderer.autoClear = true;
+    annotationSpriteMaterial.opacity = 0.2;
+    annotationSpriteMaterial.depthTest = false;
     renderer.render(scene, camera);
+
+    // render only front sprites
+    renderer.autoClear = false;
+    annotationSpriteMaterial.opacity = 1;
+    annotationSpriteMaterial.depthTest = true;
+    renderer.render(sprites, camera);
 }
 animate();
 
