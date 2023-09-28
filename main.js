d1cee289953a29ffc1358cd3ee4f9e2ab1c31301
ayyPls <saispaev55@mail.ru>
2023-09-28 17:25:46 +0700

    test

--- main.js
+++ main.js
@@ -1,167 +1,202 @@
-import { Vector2 } from 'three';
+import { BufferGeometry, CubicBezierCurve3, GridHelper, Line, LineBasicMaterial, PointLight, SRGBColorSpace, Vector2 } from 'three';
 import { Raycaster, Vector3 } from 'three';
 import { Scene, PerspectiveCamera, AxesHelper, WebGLRenderer, AmbientLight, AnimationMixer, Clock, Color, SpriteMaterial, Sprite, CanvasTexture, Group } from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import Stats from 'three/examples/jsm/libs/stats.module'
-import { CSS2DObject, CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer'
-// TODO: наезд камеры при первом рендере 
-// TODO: background of scene (skybox?) https://threejs.org/manual/#en/backgrounds
 
-let mixer, floatingAnimation, shootingAnimation;
 
-const clock = new Clock()
+// https://convert3d.org/convert/fbx
+// TODO: move in scene using scroll/ add scrub or ui to move between points and annotations on objects when you are on point
+let scrollPercent = 0
 
 const scene = new Scene();
 scene.background = new Color(0xc5c5c5) // set scene background color
+// Функция для создания кривой Безье в 3D-пространстве
+function createBezierCurve(points) {
+    // Создаем экземпляр класса THREE.CubicBezierCurve3
+    const curve = new CubicBezierCurve3(
+        points[0], // Начальная точка
+        points[1], // Контрольная точка 1
+        points[2], // Контрольная точка 2
+        points[3]  // Конечная точка
+    );
 
-const camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    // Создаем геометрию для кривой Безье
+    const curveGeometry = new BufferGeometry().setFromPoints(curve.getPoints(100)); // 100 - количество точек на кривой
+
+    // Создаем материал для кривой
+    const curveMaterial = new LineBasicMaterial({ color: 0xff0000 });
+
+    // Создаем объект кривой
+    const curveObject = new Line(curveGeometry, curveMaterial);
+
+    // Возвращаем объект кривой
+    return curveObject;
+}
+
+const points = [
+    new Vector3(-2, 1, 1), // Начальная точка
+    new Vector3(-1, 3, 0), // Контрольная точка 1
+    new Vector3(1, -3, -2), // Контрольная точка 2
+    new Vector3(1, 2, 0),  // Конечная точка
+    new Vector3(1, 2, 0),  // Конечная точка
+    new Vector3(0, 2, -3),  // Конечная точка
+    new Vector3(-1, 2, -5),  // Конечная точка
+    new Vector3(-2, 2, -7),  // Конечная точка
+];
+
+const bezierCurve = createBezierCurve(points.slice(0, 3));
+const bezierCurve2 = createBezierCurve(points.slice(4));
+
+scene.add(bezierCurve);
+scene.add(bezierCurve2);
+const curve = new CubicBezierCurve3(
+    points[0], // Начальная точка
+    points[1], // Контрольная точка 1
+    points[2], // Контрольная точка 2
+    points[3]  // Конечная точка
+);
+const curve2 = new CubicBezierCurve3(
+    points[4], // Начальная точка
+    points[5], // Контрольная точка 1
+    points[6], // Контрольная точка 2
+    points[7]  // Конечная точка
+);
+// Used to fit the lerps to start and end at specific scrolling percentages
+function scalePercent(start, end) {
+    return (scrollPercent - start) / (end - start)
+}
+const animationScripts = []
+animationScripts.push({
+    start: 0,
+    end: 49,
+    func: () => {
+        const point = curve.getPointAt(scalePercent(0, 49));
+        camera.position.copy(point);
+        camera.lookAt(getElementByName(scene, "head1").position)
+    },
+})
+animationScripts.push({
+    start: 50,
+    end: 101,
+    func: () => {
+        const point = curve2.getPointAt(scalePercent(50, 101));
+        camera.position.copy(point);
+        camera.lookAt(getElementByName(scene, "head1").position)
+    },
+})
+
+
+function playScrollAnimations() {
+    animationScripts.forEach(a => {
+        if (scrollPercent >= a.start && scrollPercent < a.end) {
+            a.func()
+        }
+    })
+}
+document.body.onscroll = () => {
+    //calculate the current scroll progress as a percentage
+    scrollPercent =
+        ((document.documentElement.scrollTop || document.body.scrollTop) /
+            ((document.documentElement.scrollHeight ||
+                document.body.scrollHeight) -
+                document.documentElement.clientHeight)) *
+        100;
+    console.log(scrollPercent.toFixed(3));
+}
+
+function getElementByName(object3d, name) {
+    let result
+    object3d.traverse(obj => {
+        if (obj.name == name) {
+            result = obj
+            return;
+        }
+    })
+
+    return result
+}
+
+let mixer
+
+const clock = new Clock()
+
+
+const camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
+const cameraGroup = new Group()
+cameraGroup.name = "cameraGroup"
+scene.add(cameraGroup)
+cameraGroup.add(camera)
+
 // set camera position
-camera.position.x = -3
-camera.position.y = 1
-camera.position.z = -3
+camera.position.x = -2
+camera.position.y = 10
+camera.position.z = -2
+
+const gridHelper = new GridHelper()
+scene.add(gridHelper)
 
 const axesHelper = new AxesHelper(5);
-// scene.add(axesHelper);
+scene.add(axesHelper);
 
-// const stats = new Stats()
-// document.body.appendChild(stats.dom)
-
-
-const renderer = new WebGLRenderer();
+const renderer = new WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.domElement.className = 'renderer'
 document.body.appendChild(renderer.domElement);
 
-const annotationRenderer = new CSS2DRenderer();
-annotationRenderer.setSize(window.innerWidth, window.innerHeight);
-annotationRenderer.domElement.className = 'annotationRenderer'
-annotationRenderer.domElement.style.position = 'absolute'
-annotationRenderer.domElement.style.top = 0
-document.body.appendChild(annotationRenderer.domElement);
+const light = new AmbientLight()
+scene.add(light)
 
-const light = new AmbientLight(0x404040, 100); // soft white light
-scene.add(light);
-
-const controls = new OrbitControls(camera, annotationRenderer.domElement) // bind controls to 2d renderer to work if you have some 2d to render
+const controls = new OrbitControls(camera, renderer.domElement) // bind controls to 2d renderer to work if you have some 2d to render
 controls.enablePan = false; // disable moving model with ctrl+mouse
 controls.enableDamping = true; // smooth camera rotation
 controls.dampingFactor = 0.05;
-controls.maxDistance = 5; // min and max distance for camera zoom
-controls.minDistance = 1.5;
+// controls.enableZoom = false
 
-let canvas = document.createElement('canvas');
-canvas.width = 64;
-canvas.height = 64; let context = canvas.getContext('2d');
-context.lineStyle = 'black';
-context.lineWidth = 6;
-context.fillStyle = 'white';
-context.beginPath();
-context.arc(32, 32, 24, 0, 2 * Math.PI);
-context.fill();
-context.stroke();
-
-const circleTexture = new CanvasTexture(canvas)
-const annotationSpriteMaterial = new SpriteMaterial({
-    transparent: true,
-    opacity: 1,
-    depthTest: true,
-    map: circleTexture,
-})
-
-const modelUrl = '/models/wither_boss/source/witherBoss.gltf';
-
+const modelUrl = '/models/Scene21/result.gltf';
+renderer.outputColorSpace = SRGBColorSpace;
 
 const loader = new GLTFLoader()
+// const loader = new FBXLoader()
 loader.load(
     modelUrl,
     model => {
         mixer = new AnimationMixer(model.scene)
-        // bind animations
-        floatingAnimation = model.animations[0]
-        shootingAnimation = model.animations[1]
-        mixer.clipAction(floatingAnimation).play().timeScale = 0.5
-        model.scene.position.y = -1.3
-        attachAnnotationSprites(model)
+        model.scene.scale.set(0.003, 0.003, 0.003)
+        // model.scene.position.setY(-2)
+        // controls.target = getElementByName(model.scene, "head1").position //sets orbit controls target
         scene.add(model.scene)
-        console.log(scene);
-        console.log(spritesArray);
     },
     () => {
         // on progress
     },
     error => console.error(error)
 )
-var spritesArray = [];
 
-function attachAnnotationSprites(model) {
-    model.scene.children.map(obj => obj.children.filter(part => !part.isMesh).map((part, index) => {
-        const annotationSprite = new Sprite(annotationSpriteMaterial)
-        annotationSprite.scale.set(0.05, 0.05, 0.05)
-        annotationSprite.position.copy({ ...part.position, y: part.position.y - 1, z: part.position.z - 0.3 })
-        part.attach(annotationSprite)
-        spritesArray.push(annotationSprite)
-    }))
-}
-
-function renderAnnotationSprites(renderer) {
-    // render scene + all sprites as transparent
-    renderer.autoClear = true;
-    annotationSpriteMaterial.opacity = 0.2;
-    annotationSpriteMaterial.depthTest = false;
-    renderer.render(scene, camera);
-
-    // render only front sprites
-    renderer.autoClear = false;
-    annotationSpriteMaterial.opacity = 1;
-    annotationSpriteMaterial.depthTest = true;
-    spritesArray.map((sprite) => { renderer.render(sprite, camera); })
-    annotationRenderer.render(scene, camera)
-}
+const cursor = { x: 0, y: 0 }
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / window.innerWidth - 0.5
+    cursor.y = event.clientY / window.innerHeight - 0.5
+})
 
 function animate() {
     requestAnimationFrame(animate);
     const delta = clock.getDelta()
     controls.update()
-    // stats.update();
+
+    // const parallaxX = cursor.x * 0.5
+    // const parallaxY = - cursor.y * 0.5
+    // cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * delta
+    // cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * delta
+    // playScrollAnimations()
 
     // run animation
     if (mixer) mixer.update(delta)
 
-    renderAnnotationSprites(renderer)
+    renderer.render(scene, camera)
 }
 animate();
-
-
-let openedAnnotation;
-const onWindowClick = (e) => {
-    e.preventDefault()
-    if (openedAnnotation) {
-        scene.remove(openedAnnotation)
-    }
-    const raycaster = new Raycaster()
-    let mouse = new Vector2();
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-    raycaster.setFromCamera(mouse, camera)
-
-    const intersects = raycaster.intersectObjects(scene.children);
-    for (let i = 0; i < intersects.length; i++) {
-        // You can do anything you want here, this is just an example to make the hovered object transparent
-        const intersectedObject = intersects[i].object
-        if (intersectedObject.type == 'Sprite') {
-            const annotationContainer = document.createElement('div')
-            annotationContainer.id = 'annotationContainer'
-            annotationContainer.innerText = intersectedObject.parent.name
-            
-            openedAnnotation = new CSS2DObject(annotationContainer)
-            openedAnnotation.position.copy(intersectedObject.parent.position)
-            openedAnnotation.position.setY(intersectedObject.parent.position.y - 0.8)
-            openedAnnotation.position.setZ(intersectedObject.parent.position.z - 0.3)
-            scene.add(openedAnnotation)
-        }
-    }
-}
 
 const onWindowResize = () => {
     camera.aspect = window.innerWidth / window.innerHeight
@@ -170,5 +205,5 @@ const onWindowResize = () => {
     // to prevent blurring output canvas ?
     renderer.setPixelRatio(window.devicePixelRatio);
 }
+
 window.addEventListener('resize', onWindowResize, false)
-window.addEventListener('click', onWindowClick, false)
